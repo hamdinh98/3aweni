39ccb80adae8378adee0d8ec9a498bc45bf5d267
hamdinh98 <hamdinahdi2@gmail.com>
2022-02-28 21:57:56 -1200

    fixing remote database connection

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const bookRoutes = require('./routes/BookApi')
 const app = express();
 
 // Connect Database
 connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+app.use(bookRoutes)
 
 const port = 5000;
 
