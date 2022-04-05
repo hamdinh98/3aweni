8694102de35dc49fbfffb6d742f27a418e5c0bcc
Tarek <xtar3k@gmail.com>
2022-04-05 23:34:27 +0200

    logo

--- front-end/src/parts/Home/Header.js
+++ front-end/src/parts/Home/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
 
                     <div className="navbar navbar-light">
 
-                        <div className="site-logo"><Link to="/"><img src="/assets/img/logo" alt="Funden" /></Link>
+                        <div className="site-logo"><Link to="/"><img src="/assets/img/logo/logo_small.png" alt="Funden" /></Link>
                         </div>
 
                         <NavbarMenu />
